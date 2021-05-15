dee39eaa01d835b07707426c3ea0cd7949d4a334
ABonokhov <batontrojan@mail.ru>
2021-05-15 15:50:08 +0300

    lab9_final

---
+++ WinTimer1/UserControl1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WinTimer1
+{
+    public partial class UserControlTimer : UserControl
+    {
+        public UserControlTimer()
+        {
+            InitializeComponent();
+        }
+
+        private void UserControl1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Text = DateTime.Now.ToLongTimeString();
+        }
+        public bool TimeEnabled
+        {
+            get { return timer1.Enabled; }
+            set { timer1.Enabled = value; }
+        }
+    }
+}
