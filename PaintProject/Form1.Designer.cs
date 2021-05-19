563c73418bf32f2354382e091aaf1b55ddc589b0
ABonokhov <batontrojan@mail.ru>
2021-05-20 00:00:35 +0300

    Finall_Comm_Paint_project

---
+++ PaintProject/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿
+namespace PaintProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.picDrawingSurface = new System.Windows.Forms.PictureBox();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.saveDlg = new System.Windows.Forms.SaveFileDialog();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picDrawingSurface)).BeginInit();
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.editToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveToolStripMenuItem,
+            this.newToolStripMenuItem,
+            this.openToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.newToolStripMenuItem.Text = "New";
+            this.newToolStripMenuItem.Click += new System.EventHandler(this.newToolStripMenuItem_Click);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.editToolStripMenuItem.Text = "Edit";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // picDrawingSurface
+            // 
+            this.picDrawingSurface.Location = new System.Drawing.Point(105, 24);
+            this.picDrawingSurface.Name = "picDrawingSurface";
+            this.picDrawingSurface.Size = new System.Drawing.Size(695, 383);
+            this.picDrawingSurface.TabIndex = 1;
+            this.picDrawingSurface.TabStop = false;
+            this.picDrawingSurface.MouseMove += new System.Windows.Forms.MouseEventHandler(this.picDrawingSurface_MouseMove);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabel1,
+            this.toolStripButton1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 24);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(800, 25);
+            this.toolStrip1.TabIndex = 2;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(86, 22);
+            this.toolStripLabel1.Text = "toolStripLabel1";
+            this.toolStripLabel1.Click += new System.EventHandler(this.toolStripLabel1_Click);
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton1.Text = "toolStripButton1";
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(119, 291);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 100);
+            this.panel1.TabIndex = 5;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(551, 405);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(249, 45);
+            this.trackBar1.TabIndex = 4;
+            // 
+            // saveDlg
+            // 
+            this.saveDlg.FileOk += new System.ComponentModel.CancelEventHandler(this.saveDlg_FileOk);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.picDrawingSurface);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picDrawingSurface)).EndInit();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.PictureBox picDrawingSurface;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.SaveFileDialog saveDlg;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ImageList imageList1;
+    }
+}
+
---
+++ PaintProject/Form1.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PaintProject
+
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+
+            InitializeComponent();
+            bool drawing;
+            int historyCounter;
+            GraphicsPath currentPath;
+            Point oldLocation;
+            Pen currentPen;
+            drawing = false; //Переменная, ответственная за рисование
+            currentPen = new Pen(Color.Black); //Инициализация пера с черным цветом
+            Color historyColor;
+            List<Image> History;
+            
+        }
+
+        private void picDrawingSurface_MouseDown(object sender, MouseEventArgs e)
+        {
+            List<Image> History = new List<Image>();
+            bool drawing;
+            GraphicsPath currentPath;
+            Point oldLocation;
+            Pen currentPen;
+            drawing = false; //Переменная, ответственная за рисование
+            currentPen = new Pen(Color.Black); //Инициализация пера с черным цветом
+
+            if (picDrawingSurface.Image == null)
+            {
+                MessageBox.Show("Сначала создайте новый файл!");
+                return;
+                if (picDrawingSurface.Image == null)
+                {
+                    MessageBox.Show("Сначала создайте новый файл!");
+                    return;
+                }
+                if (e.Button == MouseButtons.Left)
+                {
+                    drawing = true;
+                    oldLocation = e.Location;
+                    currentPath = new GraphicsPath();
+                }
+
+
+            }
+
+
+        }
+
+        private void saveDlg_FileOk(object sender, CancelEventArgs e)
+        {
+            SaveFileDialog SaveDlg = new SaveFileDialog();
+            SaveDlg.Filter = "JPEG Image|*.jpg|Bitmap Image|*.bmp|GIF Image|*.gif|PNGImage | *.png";
+            SaveDlg.Title = "Save an Image File";
+            SaveDlg.FilterIndex = 4; //По умолчанию будет выбрано последнее расширение*.png
+            SaveDlg.ShowDialog();
+
+            if (SaveDlg.FileName != "") //Если введено не пустое имя
+            {
+                System.IO.FileStream fs = (System.IO.FileStream)SaveDlg.OpenFile();
+                switch (SaveDlg.FilterIndex)
+                {
+                    case 1:
+                        this.picDrawingSurface.Image.Save(fs,
+                        ImageFormat.Jpeg);
+                        break;
+                    case 2:
+                        this.picDrawingSurface.Image.Save(fs,
+                        ImageFormat.Bmp);
+                        break;
+                    case 3:
+                        this.picDrawingSurface.Image.Save(fs,
+                        ImageFormat.Gif);
+                        break;
+                    case 4:
+                        this.picDrawingSurface.Image.Save(fs,
+                        ImageFormat.Png);
+                        break;
+                }
+                fs.Close();
+
+
+
+            }
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (picDrawingSurface.Image != null)
+            {
+                var result = MessageBox.Show("Сохранить текущее изображение передсозданием нового рисунка ? ", "Предупреждение",
+                MessageBoxButtons.YesNoCancel);
+                switch (result)
+                {
+                    case DialogResult.No: break;
+                    case DialogResult.Yes: saveMenu_Click(sender, e); break;
+                    case DialogResult.Cancel: return;
+                }
+            }
+            Graphics g = Graphics.FromImage(picDrawingSurface.Image);
+            g.Clear(Color.White);
+            g.DrawImage(picDrawingSurface.Image, 0, 0, 750, 500);
+        }
+
+        private void saveMenu_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Bitmap pic = new Bitmap(750, 500);
+            picDrawingSurface.Image = pic;
+            if (picDrawingSurface.Image != null)
+            {
+                var result = MessageBox.Show("Сохранить текущее изображение передсозданием нового рисунка ? ", "Предупреждение",
+                MessageBoxButtons.YesNoCancel);
+                switch (result)
+                {
+                    case DialogResult.No: break;
+                    case DialogResult.Yes: saveMenu_Click(sender, e); break;
+                    case DialogResult.Cancel: return;
+                }
+            }
+            List<Image> History = new List<Image>();
+            int historyCounter;
+            History.Clear();
+            historyCounter = 0;
+            picDrawingSurface.Image = pic;
+            History.Add(new Bitmap(picDrawingSurface.Image));
+
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog OP = new OpenFileDialog();
+            OP.Filter = "JPEG Image|*.jpg|Bitmap Image|*.bmp|GIF Image|*.gif|PNGImage | *.png";
+            OP.Title = "Open an Image File";
+            OP.FilterIndex = 1; //По умолчанию будет выбрано первое расширение *.jpg
+
+            if (OP.ShowDialog() != DialogResult.Cancel)
+                picDrawingSurface.Load(OP.FileName);
+            picDrawingSurface.AutoSize = true;
+        }
+
+        private void toolStripLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+
+
+        private void picDrawingSurface_MouseUp(object sender, MouseEventArgs e)
+        {
+            bool drawing;
+            GraphicsPath currentPath;
+            drawing = false;
+            currentPath = new GraphicsPath();
+            List<Image> History = new List<Image>();
+            int historyCounter;
+            History.Clear();
+            historyCounter = 0;
+            try
+            {
+                currentPath.Dispose();
+            }
+            catch { };
+
+            History.RemoveRange(historyCounter + 1, History.Count - historyCounter - 1);
+            History.Add(new Bitmap(picDrawingSurface.Image));
+            if (historyCounter + 1 < 10) historyCounter++;
+            if (History.Count - 1 == 10) History.RemoveAt(0);
+            drawing = false;
+            try
+            {
+                currentPath.Dispose();
+            }
+            catch { };
+
+        }
+
+        private void picDrawingSurface_MouseMove(object sender, MouseEventArgs e)
+        {
+            bool drawing;
+            GraphicsPath currentPath;
+            Point oldLocation;
+            Pen currentPen;
+            drawing = false; //Переменная, ответственная за рисование
+            currentPen = new Pen(Color.Black); //Инициализация пера с черным цветом
+            if (drawing)
+            {
+                Graphics g = Graphics.FromImage(picDrawingSurface.Image);
+                oldLocation = e.Location;
+                g.Dispose();
+                picDrawingSurface.Invalidate();
+            }
+
+        }
+
+        
+
+        private void undoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            List<Image> History = new List<Image>();
+            int historyCounter;
+            History.Clear();
+            historyCounter = 0;
+            if (History.Count != 0 && historyCounter != 0)
+            {
+                picDrawingSurface.Image = new Bitmap(History[--historyCounter]);
+            }
+            else MessageBox.Show("История пуста");
+        }
+
+        private void renoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            List<Image> History = new List<Image>();
+            int historyCounter;
+            History.Clear();
+            historyCounter = 0;
+            if (historyCounter < History.Count - 1)
+            {
+                picDrawingSurface.Image = new Bitmap(History[++historyCounter]);
+            }
+            else MessageBox.Show("История пуста");
+        }
+
+    }
+
+
+}
+
